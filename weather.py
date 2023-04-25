7561c39dc032a86f0b457c60fe5d9097aa28fc57
kwon-evan <koun012@naver.com>
2023-04-25 17:55:38 +0900

    fix function using 'wrttr.in'

--- weather.py
+++ weather.py
@@ -1,70 +1,22 @@
 import asyncio
 
-from bs4 import BeautifulSoup
-import urllib.request as req
-import ssl
-
-kor2eng = {
-    "미세먼지": "dust",
-    "초미세먼지": "ultra_dust",
-    "자외선": "uv",
-    "좋음": "GOOD",
-    "보통": "NORMAL",
-    "나쁨": "BAD",
-    "높아요": "+",
-    "낮아요": "-",
-}
+import httpx
+from rich.text import Text
+from rich import print
 
 
-async def analyze_summary(summary):
-    _, temp_diff, up_down, stats = summary.text.split()
-    up_down = kor2eng[up_down]
-    temp_diff = up_down + temp_diff + "C"
-    return {"temp_diff": temp_diff, "stats": stats}
+async def update_weather(city: str) -> Text:
+    url = f"https://wttr.in/?0nQF&lang=ko"
 
-
-async def analyze_today_chart(today_chart_list):
-    result = {}
-    for i in today_chart_list:
-        title = i.find("strong", "title").text.strip()
-        txt = i.find("span", "txt").text.strip()
-        try:
-            result[kor2eng[title]] = kor2eng[txt]
-        except KeyError:
-            pass
-    return result
-
-
-async def analyze_current(soup):
-    return {
-        "location": soup.find("h2", "title").text.strip(),
-        "temps": soup.find("div", "temperature_text").text.strip().strip("현재 온도") + "C",
-    }
-
-
-async def get_weather():
-    result = {}
-    loop = asyncio.get_event_loop()
-
-    webpage = req.urlopen(
-        "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=%EB%82%A0%EC%94%A8",
-        context=ssl._create_unverified_context(),
-    )
-    soup = BeautifulSoup(webpage, "html.parser")
-
-    today_chart_list = loop.create_task(analyze_today_chart(soup.find("ul", "today_chart_list").findAll("li")))
-    summary = loop.create_task(analyze_summary(soup.find("p", "summary")))
-    current = loop.create_task(analyze_current(soup))
-
-    await asyncio.wait([today_chart_list, summary, current])
-
-    result.update(**current.result(), **today_chart_list.result(), **summary.result())
-
-    return result
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        txt = Text.from_ansi(response.text)
+        return txt
 
 
 async def main():
-    ret = await get_weather()
+    # ret = await get_weather()
+    ret = await update_weather("Gumi")
     print(ret)
 
 
